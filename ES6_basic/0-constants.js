73ecddc6cc770dde238c38a1d74b39ee91eb6f78
Nomad-Rob <rfarley89@gmail.com>
2023-09-06 13:00:59 -0500

    trying without comments. having issues with the linter

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,17 +1,14 @@
-// Function to return a task description.
 export function taskFirst() {
-  const task = 'I prefer const when I can.'; // Declare a constant variable 'task' with a string value.
-  return task; // Return the 'task' string.
+  const task = 'I prefer const when I can.';
+  return task;
 }
 
-// Function to return a fixed message.
 export function getLast() {
-  return ' is okay'; // Return the string ' is okay'.
+  return ' is okay';
 }
 
-// Function to combine a predefined string with the result of 'getLast'.
 export function taskNext() {
-  let combination = 'But sometimes let'; // Declare a variable 'combination' using 'let'.
-  combination += getLast(); // Append the result of 'getLast' to 'combination'.
-  return combination; // Return the combined string.
+  let combination = 'But sometimes let';
+  combination += getLast();
+  return combination;
 }
