b4f64c4af9725ac5b8c241e154905d604865f42d
Nomad-Rob <rfarley89@gmail.com>
2023-09-13 17:10:07 -0500

    adding new comments

--- ES6_promise/commented/9-Commented.js
+++ ES6_promise/commented/9-Commented.js
@@ -8,23 +8,22 @@
 // when this module is imported in other parts of the code.
 
 export default function guardrail(mathFunction) {
-    // Create an empty array 'queue' to store the results and messages.
-    const queue = [];
-  
-    try {
-      // Try to execute the provided 'mathFunction' and push its result into the 'queue'.
-      queue.push(mathFunction());
-    } catch (e) {
-      // If an error occurs during the execution of 'mathFunction',
-      // catch the error and push an error message into the 'queue'.
-      queue.push('Error:'.concat(e.message));
-    } finally {
-      // Regardless of whether 'mathFunction' succeeded or failed,
-      // push a message into the 'queue' to indicate that the guardrail was processed.
-      queue.push('Guardrail was processed');
-    }
-  
-    // Return the 'queue' array, which contains the results and messages.
-    return queue;
+  // Create an empty array 'queue' to store the results and messages.
+  const queue = [];
+
+  try {
+    // Try to execute the provided 'mathFunction' and push its result into the 'queue'.
+    queue.push(mathFunction());
+  } catch (e) {
+    // If an error occurs during the execution of 'mathFunction',
+    // catch the error and push an error message into the 'queue'.
+    queue.push(`Error: ${e.message}`);
+  } finally {
+    // Regardless of whether 'mathFunction' succeeded or failed,
+    // push a message into the 'queue' to indicate that the guardrail was processed.
+    queue.push('Guardrail was processed');
   }
-  
+
+  // Return the 'queue' array, which contains the results and messages.
+  return queue;
+}
